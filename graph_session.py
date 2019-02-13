8a4ba327eec29b1de4efbe31d57512469e28b72a
Sagor Sarker <sagorhem3532@gmail.com>
2019-02-13 15:52:10 +0600

    Update graph_session.py

--- graph_session.py
+++ graph_session.py
@@ -1,14 +1,16 @@
 # import tensorflwo
 import tensorflow as tf
+
+#Graph Part
 x = 2
 y = 3
-
 #simple mathematical operation using tensorflow
 add_op = tf.add(x, y, name='Add')
 mul_op = tf.multiply(x, y, name='Multiply')
 pow_op = tf.pow(add_op, mul_op, name='Power')
 useless_op = tf.multiply(x, add_op, name='Useless')
 
+#Session Part
 with tf.Session() as sess:
     pow_out, useless_out = sess.run([pow_op, useless_op])
     # to visualize tensorboard
