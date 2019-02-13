b6aef81567ef00d6bda7a9d50e2d1486e44992a2
Sagor Sarker <sagorhem3532@gmail.com>
2019-02-13 15:51:35 +0600

    Add files via upload

---
+++ graph_session.py
@@ -0,0 +1,16 @@
+# import tensorflwo
+import tensorflow as tf
+x = 2
+y = 3
+
+#simple mathematical operation using tensorflow
+add_op = tf.add(x, y, name='Add')
+mul_op = tf.multiply(x, y, name='Multiply')
+pow_op = tf.pow(add_op, mul_op, name='Power')
+useless_op = tf.multiply(x, add_op, name='Useless')
+
+with tf.Session() as sess:
+    pow_out, useless_out = sess.run([pow_op, useless_op])
+    # to visualize tensorboard
+    # run: tensorboard --logdir=graph --port=6006
+    writer = tf.summary.FileWriter('/graph', sess.graph)
